f08f0722307822a11af875197fa1d10670bc2897
ScottSILS <smurray@sasklibraries.ca>
2014-06-26 13:45:22 -0600

    checking some js types..

--- js/sils_search.js
+++ js/sils_search.js
@@ -1,10 +1,11 @@
+//do we even have jquery up in here? can't use $('.whatevs')
 if (typeof jQuery == 'undefined') {
-    // jQuery is not loaded
     console.log("jQuery NOT LOADED");
     } else {
     console.log("jQuery is loaded successfully!");
     }
 
+//but apparently calling jQuery(); works just dandy
 jQuery( document ).ready(function() {
 
 var libSearchArray = [];
@@ -12,6 +13,14 @@ var libSearchArray = [];
 jQuery(".libLocation").each( function(index, element){
     libSearchArray.push( jQuery( this ).text() );
 });
+
+
+//figuring out what we're working with
+console.log(typeof(libSearchArray));
+
+if (libSearchArray.jquery) {
+    console.log("libSearchArray is indeed a jquery object");
+};
 
 console.log(libSearchArray);
 
