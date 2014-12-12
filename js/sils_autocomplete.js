f96091305b25c5f9015d0c58d58a341aff1c9ea4
ScottSILS <smurray@sasklibraries.ca>
2014-12-12 12:24:21 -0600

    fixing autocomplete endpoint round 2

--- js/sils_autocomplete.js
+++ js/sils_autocomplete.js
@@ -4,7 +4,7 @@ var searchResults = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   remote: {
-            url: '/sils/eds/ajax/suggestions/%QUERY',
+            url: '/sils/eds/ajax/suggestions/?search=%QUERY',
 	    filter: function(parsedResponse) {
             var dataset = [];
             for(i = 0; i < parsedResponse.length; i++) {
